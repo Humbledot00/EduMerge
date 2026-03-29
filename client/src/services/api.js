0a93fbbbe5da8bf6649d3250d4dee374ca5a7780
Shreyas Rao <109731624+Humbledot00@users.noreply.github.com>
2026-03-29 23:30:07 +0530

    Update API base URL to production endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://edu-merge-4y22.vercel.app/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
